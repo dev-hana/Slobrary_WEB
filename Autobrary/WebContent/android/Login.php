62bc2f4260ef0fc649f41738a16fb3f3dbcaf733
comghana <ji970322@naver.com>
2020-04-21 00:15:11 +0900

    update Login.php;

--- Autobrary/WebContent/android/Login.php
+++ Autobrary/WebContent/android/Login.php
@@ -1,23 +1,25 @@
 <?php	//중복방지문
+
 	$con = mysqli_connect("www.slobrary.com", "slo", "legoslo2020", "slo");	//아이피,아이디,비번,디비이름
 	
 	$ID = $_POST["ID"];
+	
+	
 	$statement = mysqli_prepare($con, "SELECT * FROM member WHERE MEM_ID = ?");	//테이블안아이디확인
 	mysqli_stmt_bind_param($statement, "s", $ID);	
 	mysqli_stmt_execute($statement);
-
-	mysqli_stmt_store_result($statement);
-	mysqli_stmt_bind_result($statement, $ID, $PASSWD);
+    $result = mtsqli_stmt_get_result($statement);
 
 	$response = array();	
 	$response["success"] = false;
 
-	while(mysqli_stmt_fetch($statement)){
+	while($row = mysqli_fetch_assoc($result)){
 		$response["success"] = true;
 		$response["MEM_ID"] = $ID;
-		$PASSWD = $response["PASSWD"];
+		$response["PASSWD"]= $row["PASSWD"];
 	}
 	
 	
 	echo json_encode($response);
+	mysqli_close($con);
 ?>
