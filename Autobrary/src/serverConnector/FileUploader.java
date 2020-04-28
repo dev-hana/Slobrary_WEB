36d36e71300a5394757ad5d251f2ccdd5a7194d6
sangik-lee <dev_sang@naver.com>
2020-04-28 23:27:24 +0900

    minio 객체 버킷 연동 버그 수정

--- Autobrary/src/serverConnector/FileUploader.java
+++ Autobrary/src/serverConnector/FileUploader.java
@@ -8,23 +8,11 @@ import io.minio.MinioClient;
 import io.minio.errors.MinioException;
 
 public class FileUploader {
-  public static void main(String[] args) throws NoSuchAlgorithmException, IOException, InvalidKeyException{
+  public FileUploader(String name, String path) throws NoSuchAlgorithmException, IOException, InvalidKeyException{
     try {
-      // Create a minioClient with the MinIO Server name, Port, Access key and Secret key.
-      MinioClient minioClient = new MinioClient("http://slobrary.com:9000", "slo", "legoslo2020");
-
-      // Check if the bucket already exists.
-      boolean isExist = minioClient.bucketExists("asiatrip");
-      if(isExist) {
-        System.out.println("Bucket already exists.");
-      } else {
-        // Make a new bucket called asiatrip to hold a zip file of photos.
-        minioClient.makeBucket("asiatrip");
-      }
-      minioClient.putObject("slo", "\\test.gif", "slo", null);
-      // Upload the zip file to the bucket with putObject
-      //minioClient.putObject("asiatrip","asiaphotos.zip", "/home/user/Photos/asiaphotos.zip");
-      System.out.println("/home/user/Photos/asiaphotos.zip is successfully uploaded as asiaphotos.zip to `asiatrip` bucket.");
+    	//********보안정보 취급 주의**********
+      MinioClient minioClient = new MinioClient("http://slobrary.com", 9000, "slo", "legoslo2020");
+      minioClient.putObject("slo", name, path, null);
     } catch(MinioException e) {
       System.out.println("Error occurred: " + e);
     }
