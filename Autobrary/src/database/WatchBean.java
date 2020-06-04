68e8fe5ae89b134649a6af6f726dfc13dde5d00c
jieun jang <ji970322@naver.com>
2020-06-04 15:09:18 +0900

    watch bean

---
+++ Autobrary/src/database/WatchBean.java
@@ -0,0 +1,34 @@
+package database;
+
+public class WatchBean {
+	private String id;
+	private String mem_id;
+	private String book_id;
+	private String date;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getMem_id() {
+		return mem_id;
+	}
+	public void setMem_id(String mem_id) {
+		this.mem_id = mem_id;
+	}
+	public String getBook_id() {
+		return book_id;
+	}
+	public void setBook_id(String book_id) {
+		this.book_id = book_id;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	
+
+}
