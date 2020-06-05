cc84dd410e914444294d80d3d54cf136e8a61ee0
Sang - Ik Lee <dev_sang@naver.com>
2020-06-05 11:01:26 +0900

    안드로이드 회원가입 한글 데이터 입력 인코딩 오류 해결

--- Autobrary/WebContent/android/Register.php
+++ Autobrary/WebContent/android/Register.php
@@ -10,19 +10,17 @@ $con = mysqli_connect("www.slobrary.com", "slo", "legoslo2020", "slo");	//아이
 
 $mem_id = $_POST["mem_id"];
 $passwd = $_POST["passwd"];
-$name = $_POST["name"];
+$name = rawurldecode($_POST["name"]);
 $gender = $_POST["gender"];
 $phone = $_POST["phone"];
 $birth = $_POST["birth"];
-$address = $_POST["address"];
+$address = rawurldecode($_POST["address"]);
 $email = $_POST["email"];
 $response["success"] = false;
-
 
 $statement = mysqli_prepare($con, "INSERT INTO member(mem_id, passwd, name, gender, phone, birth, address, email) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
 mysqli_stmt_bind_param($statement, "ssssssss", $mem_id, $passwd, $name, $gender, $phone, $birth, $address, $email);//ssss->string형으로 8개값받기
 $result = mysqli_stmt_execute($statement);
-
 $response = array();
 $response["success"] = true;
 
